dadc260b7e6a7f0d71472f5e14c119e97d371edb
AMutaa <piusandru@gmail.com>
2019-04-16 23:09:55 -0400

    cityDetails and test"

---
+++ enzyme_test/src/CityDetail.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class CityDetail extends Component {
+  render() {
+    return (
+      <div>
+        <h1>City</h1>
+        <h3>Boston</h3>
+        <h5>02141</h5>
+      </div>
+    )
+  }
+}
+
+export default CityDetail
